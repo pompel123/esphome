ad7e42c1d523c014ac1e3790e845be794fe8d0b3
Christer Lien <pompel123@gmail.com>
2025-11-03 12:23:31 +0100

    Refactor climate.py for MillPanelHeaterGen2

--- esphome/components/mill_panelheater_gen2/climate.py
+++ esphome/components/mill_panelheater_gen2/climate.py
@@ -1,19 +1,18 @@
 from esphome.components import climate
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import automation
 
-# Create namespace and class
+# Namespace and class definition
 mill_panelheater_gen2_ns = cg.esphome_ns.namespace("mill_panelheater_gen2")
 MillPanelHeaterGen2 = mill_panelheater_gen2_ns.class_(
     "MillPanelHeaterGen2", climate.Climate, cg.Component
 )
 
-# Configuration schema (new style, replacing deprecated CLIMATE_SCHEMA)
+# Define schema (new-style, no GenerateID)
 CONFIG_SCHEMA = climate.climate_schema(MillPanelHeaterGen2).extend(cv.COMPONENT_SCHEMA)
 
-# Register component
+# Register the component and climate
 async def to_code(config):
-    var = cg.new_Pvariable(config[cv.GenerateID()])
+    var = cg.new_Pvariable("mill_panelheater_gen2", MillPanelHeaterGen2)
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
