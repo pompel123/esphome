0bf238350653ea71118c6dc039010129fc9af6cc
Christer Lien <pompel123@gmail.com>
2025-11-03 12:22:38 +0100

    Refactor MillPanelHeaterGen2 climate component
    Refactor climate component for MillPanelHeaterGen2 by removing UART dependency and updating configuration schema.

--- esphome/components/mill_panelheater_gen2/climate.py
+++ esphome/components/mill_panelheater_gen2/climate.py
@@ -1,36 +1,19 @@
+from esphome.components import climate
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, climate
-from esphome.const import (
-  CONF_ID,
-  CONF_MAX_TEMPERATURE,
-  CONF_MIN_TEMPERATURE,
-  CONF_SUPPORTED_MODES,
-)
+from esphome import automation
 
-from esphome.components.climate import (
-    ClimateMode,
-    CONF_CURRENT_TEMPERATURE,
-)
-
-CODEOWNERS = ["@owangen"]
-
-DEPENDENCIES = ["climate", "uart"]
-
+# Create namespace and class
 mill_panelheater_gen2_ns = cg.esphome_ns.namespace("mill_panelheater_gen2")
-MillHeater = mill_panelheater_gen2_ns.class_("MillPanelHeaterGen2", uart.UARTDevice, climate.Climate, cg.Component)
+MillPanelHeaterGen2 = mill_panelheater_gen2_ns.class_(
+    "MillPanelHeaterGen2", climate.Climate, cg.Component
+)
 
-CONF_MILL_ID = "mill_id"
+# Configuration schema (new style, replacing deprecated CLIMATE_SCHEMA)
+CONFIG_SCHEMA = climate.climate_schema(MillPanelHeaterGen2).extend(cv.COMPONENT_SCHEMA)
 
-CONFIG_SCHEMA = climate.climate_schema(
-    {
-        cv.GenerateID(): cv.declare_id(MillPanelHeaterGen2),
-        cv.Required("name"): cv.string,
-    }
-).extend(cv.COMPONENT_SCHEMA)
-
+# Register component
 async def to_code(config):
-  var = cg.new_Pvariable(config[CONF_ID])
-  await cg.register_component(var, config)
-  await uart.register_uart_device(var, config)
-  await climate.register_climate(var, config)
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
